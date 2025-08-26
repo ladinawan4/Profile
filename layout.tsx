5cd83626a0c1e7831c87209d71344f030a17bc62
Ladinawan <156433148+ladinawan4@users.noreply.github.com>
2025-08-26 17:45:25 +0500

    Update layout.tsx

--- layout.tsx
+++ layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,7 +11,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+              <Analytics />
+</body>
     </html>
   )
 }
